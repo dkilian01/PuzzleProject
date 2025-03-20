a7a78b64e7e1ac394ce92c47af0df311ad16b86b
dkilian01 <damian_kilian_10@wp.pl>
2025-03-20 17:27:13 +0100

    Wstępne metody PuzzleBoard

--- PuzzleProject/PuzzleBoard.h
+++ PuzzleProject/PuzzleBoard.h
@@ -1,7 +1,21 @@
 #pragma once
 #include "PuzzleBase.h"
+#include <vector>
+
+using namespace std;
 class PuzzleBoard :
     public PuzzleBase
 {
+private:
+    vector<vector<int>> board;
+    int mx, my;
+
+public:
+    PuzzleBoard(int size);
+    void move(int x, int y) override;
+    bool isSolved() const override;
+    int getSize() { return board.size(); }
+    vector<vector<int>> getBoard() { return board; }
+
 };
 
